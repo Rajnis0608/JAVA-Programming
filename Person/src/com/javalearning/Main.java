a5dca7d9a715706e642a77a46bbd3b81ff86cd42
rajnis0608 <rajnis0608@gmail.com>
2020-12-13 00:19:33 +0530

    Classes

---
+++ Person/src/com/javalearning/Main.java
@@ -0,0 +1,22 @@
+package com.javalearning;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person person = new Person();
+	    person.setFirstName("");
+	    person.setLastName("");
+	    person.setAge(10);
+        System.out.println("fullName = " + person.getFullName());
+        System.out.println("teen = " + person.isTeen());
+        person.setFirstName("John");
+        person.setAge(18);
+        System.out.println("fullName = " + person.getFullName());
+        System.out.println("teen = " + person.isTeen());
+        person.setLastName("smith");
+        System.out.println("fullName = " + person.getFullName());
+        person.setAge(-5);
+        System.out.println(person.getAge());
+    }
+}

c07f85d67ed0cbef3aeed98875a960781dd6a104
rajnis0608 <rajnis0608@gmail.com>
2020-12-16 15:03:56 +0530

    Composition Example

---
+++ Composition/src/com/javalearning/Ceiling.java
@@ -0,0 +1,20 @@
+package com.javalearning;
+
+public class Ceiling {
+
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int printedColor) {
+        this.height = height;
+        this.paintedColor = printedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPrintedColor() {
+        return paintedColor;
+    }
+}

f031d69ada00f2cd8600b2c2efd37b84e2a32e4f
rajnis0608 <rajnis0608@gmail.com>
2020-12-16 18:32:50 +0530

    Encapsulation Example

---
+++ Encapsulation/src/com/javalearning/Main.java
@@ -0,0 +1,18 @@
+package com.javalearning;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Printer printer = new Printer(50, true);
+        System.out.println(printer.addToner(50));
+        System.out.println("initial page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+
+
+
+    }
+}

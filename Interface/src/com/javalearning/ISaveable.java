a02e263b9a85780ee2c655a6f4d9e9f6b96eced4
rajnis0608 <rajnis0608@gmail.com>
2020-12-25 01:26:35 +0530

    Interface functions

---
+++ Interface/src/com/javalearning/ISaveable.java
@@ -0,0 +1,11 @@
+package com.javalearning;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ISaveable {
+
+    public List<String> write();
+    public void read(List<String> savedValues);
+}
+

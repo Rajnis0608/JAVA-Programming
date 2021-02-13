e03f4642608a3d0a21d28c76b9293fc3200ef2b6
rajnis0608 <rajnis0608@gmail.com>
2021-02-13 16:37:03 +0530

    Binary search tree

--- Abstract Classes Challenge/src/Main.java
+++ Abstract Classes Challenge/src/Main.java
@@ -20,6 +20,28 @@ public class Main {
         list.removeItem(new Node("2"));
         list.tranverse(list.getRoot());
 
+        System.out.println("************************");
+
+
+        SearchTree tree = new SearchTree(null);
+        tree.tranverse(tree.getRoot());
+        String string = "5 7 3 9 8 2 1 0 4 6";
+        String[] data1 = stringData.split(" ");
+        for (String s : data1) {
+            tree.addItem(new Node(s));
+        }
+
+        tree.tranverse(tree.getRoot());
+        tree.removeItem(new Node("3"));
+        tree.tranverse(tree.getRoot());
+
+        tree.removeItem(new Node("5"));
+        tree.tranverse(tree.getRoot());
+
+        tree.removeItem(new Node("0"));
+        tree.removeItem(new Node("4"));
+        tree.removeItem(new Node("2"));
+        tree.tranverse(tree.getRoot());
 
 
 

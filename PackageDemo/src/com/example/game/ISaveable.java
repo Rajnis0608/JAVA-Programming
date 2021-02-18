f201518d733731abf8aae3ecb77b28c5edfe651c
rajnis0608 <rajnis0608@gmail.com>
2021-02-18 21:01:15 +0530

    Creating importable package

---
+++ PackageDemo/src/com/example/game/ISaveable.java
@@ -0,0 +1,11 @@
+package com.example.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ISaveable {
+
+    public List<String> write();
+    public void read(List<String> savedValues);
+}
+

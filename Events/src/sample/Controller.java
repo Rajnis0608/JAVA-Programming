81b4c4f9939b6c577ba1808761fb99cd56f2e0c4
rajnis0608 <rajnis0608@gmail.com>
2021-04-20 00:30:02 +0530

    Adding parameters to functions in controller abd disabling property

--- Events/src/sample/Controller.java
+++ Events/src/sample/Controller.java
@@ -1,14 +1,43 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
+
+    @FXML
+    public void initialize() {
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+
+    }
     @FXML
     private TextField nameField;
 
     @FXML
-    public void onButtonClicked(){
-        System.out.println("Hello, " + nameField.getText());
+    private Button byeButton;
+
+    @FXML
+    private Button helloButton;
+
+    @FXML
+    public void onButtonClicked(ActionEvent e) {
+        if (e.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
+        else if (e.getSource().equals(helloButton)){
+            System.out.println("Hello, " + nameField.getText());
+
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased(){
+        String string = nameField.getText();
+        boolean disable = string.isEmpty() || string.trim().isEmpty();
+        byeButton.setDisable(disable);
+        helloButton.setDisable(disable);
     }
 }

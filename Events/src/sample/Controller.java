9a0117278185b39a245ef981341abc6a20a25159
rajnis0608 <rajnis0608@gmail.com>
2021-05-03 07:29:40 +0530

    UI threads and runnable

--- Events/src/sample/Controller.java
+++ Events/src/sample/Controller.java
@@ -1,8 +1,11 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -13,14 +16,18 @@ public class Controller {
         byeButton.setDisable(true);
 
     }
+
     @FXML
     private TextField nameField;
-
     @FXML
     private Button byeButton;
-
     @FXML
     private Button helloButton;
+    @FXML
+    private CheckBox ourCheckBox;
+    @FXML
+    private Label ourLabel;
+
 
     @FXML
     public void onButtonClicked(ActionEvent e) {
@@ -29,7 +36,36 @@ public class Controller {
         }
         else if (e.getSource().equals(helloButton)){
             System.out.println("Hello, " + nameField.getText());
+        }
 
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String s = Platform.isFxApplicationThread() ? "UI Thread" : "Backgroung Thread";
+                    System.out.println("I'm going to sleep on the: " + s);
+                    Thread.sleep(10000);
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            String s = Platform.isFxApplicationThread() ? "UI Thread" : "Backgroung Thread";
+                            System.out.println("I'm updating the label on the: " + s);
+                            ourLabel.setText("We did something!");
+                        }
+                    });
+                } catch (InterruptedException event) {
+                    //We dont care about this
+                }
+            }
+        };
+
+        new Thread(task).start();
+
+
+        if(ourCheckBox.isSelected()){
+            nameField.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
         }
     }
 
@@ -39,5 +75,10 @@ public class Controller {
         boolean disable = string.isEmpty() || string.trim().isEmpty();
         byeButton.setDisable(disable);
         helloButton.setDisable(disable);
+    }
+
+    public void handleChange() {
+        System.out.println("The checkbox is " + (ourCheckBox.isSelected() ? "checked" : "not checked"));
+
     }
 }
--- Sample/src/Main.java
+++ Sample/src/Main.java
@@ -4,25 +4,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int counter=0;
-        int sum = 0;
-        while(counter < 10){
-            int i = counter + 1;
-            System.out.println("Enter number #" + i + ":");
-            boolean hasNextInt=scanner.hasNextInt();
-            if(hasNextInt){
-                int x = scanner.nextInt();
-                counter++;
-                sum += x;
-            }
-            else{
-                System.out.println("Invalid Number");
-            }
-            scanner.nextLine();
-            i++;
-        }
-        scanner.close();
-        System.out.println("Sum is " + sum);
+//        Scanner scanner = new Scanner(System.in);
+//        int counter=0;
+//        int sum = 0;
+//        while(counter < 10){
+//            int i = counter + 1;
+//            System.out.println("Enter number #" + i + ":");
+//            boolean hasNextInt=scanner.hasNextInt();
+//            if(hasNextInt){
+//                int x = scanner.nextInt();
+//                counter++;
+//                sum += x;
+//            }
+//            else{
+//                System.out.println("Invalid Number");
+//            }
+//            scanner.nextLine();
+//            i++;
+//        }
+//        scanner.close();
+//        System.out.println("Sum is " + sum);
+
+
+        System.out.println("Hello World \rMy name is Rajni");
+        System.out.println("Hello World \r");
+
+
     }
 }
+

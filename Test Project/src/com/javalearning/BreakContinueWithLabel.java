a94cabe60005da85316a807159290c1dab176162
rajnis0608 <rajnis0608@gmail.com>
2021-07-06 23:44:46 +0530

    Creating a text file and writing content to file using code

--- Test Project/src/com/javalearning/BreakContinueWithLabel.java
+++ Test Project/src/com/javalearning/BreakContinueWithLabel.java
@@ -1,23 +1,40 @@
 package com.javalearning;
 
 public class BreakContinueWithLabel {
-    public static void main(String args[]) {
-
-        int[] numbers= new int[]{100,18,21,30};
-
-        
-        for(int i = 0; i<numbers.length; i++){
-            if(i % 2 == 0){
-                System.out.println("Odd number: " + i + ", continue from OUTER label");
-                continue;
-            }
-
-            for(int j = 0; j<numbers.length; j++){
-                System.out.println("Even number: " + i + ", break  from INNER label");
-                break;
-            }
+    public static void main(String[] args) throws Throwable
+    {
+        BreakContinueWithLabel breakCont = new BreakContinueWithLabel();
+        try {
+            breakCont.method();
         }
-        
+        catch (Exception e) {
+            /*System.out.println("Exception details without fillInStackTrace()\n");
+            System.err.println("Caught Inside Main:");
+            e.printStackTrace();
+            */
+            System.out.println("Exception details with fillInStackTrace()\n");
+            System.err.println("Caught Inside Main:");
+            e.fillInStackTrace();
+            e.printStackTrace();
+            
+        }
+    }
 
+    // method calling divide operation
+    public void method() throws Throwable
+    {
+        divide();
+    }
+
+    // divide operation throws ArithmeticException exception
+    void divide()
+    {
+
+        try {
+            System.out.println(10 / 0);
+        }
+        catch (ArithmeticException e) {
+            throw e;
+        }
     }
 }
